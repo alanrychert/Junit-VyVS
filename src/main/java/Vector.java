6d29425dd0dd35eed117dc986a8e804eeac42ddf
alanrychert <alanrychert@gmail.com>
2021-10-29 17:04:40 -0300

    JUnit tests

---
+++ src/main/java/Vector.java
@@ -0,0 +1,29 @@
+public class Vector {
+
+    public int greater (int[] array){
+        int greater=Integer.MIN_VALUE;
+        for (int n: array) {
+            if (n>greater)
+                greater=n;
+        }
+        return greater;
+    }
+
+    public int minor(int[] array){
+        int minor=Integer.MAX_VALUE;
+        for (int n: array) {
+            if (n<minor)
+                minor=n;
+        }
+        return minor;
+    }
+    
+    public int sumatory(int[] array){
+        int sum=0;
+        for (int n: array
+             ) {
+            sum+=n;
+        }
+        return sum;
+    }
+}
---
+++ src/test/java/VectorTest.java
@@ -0,0 +1,77 @@
+import org.junit.jupiter.api.*;
+
+public class VectorTest {
+    private final Vector vector = new Vector();
+
+    @Test
+    public void firstElementGreaterTest(){
+        int[] array = new int[]{8,2,5,3,4,-5};
+
+        int result = vector.greater(array);
+        Assertions.assertEquals(8,result);
+
+    }
+
+    @Test
+    public void middleElementGreaterTest(){
+        int[] array = new int[]{1,2,5,3,4,-5};
+
+        int result = vector.greater(array);
+        Assertions.assertEquals(5,result);
+
+    }
+
+    @Test
+    public void lastElementGreaterTest(){
+        int[] array = new int[]{1,2,5,3,4,15};
+
+        int result = vector.greater(array);
+        Assertions.assertEquals(15,result);
+
+    }
+
+    @Test
+    public void firstElementMinorTest(){
+        int[] array = new int[]{-10,2,5,3,4,-5};
+
+        int result = vector.minor(array);
+        Assertions.assertEquals(-10,result);
+
+    }
+
+    @Test
+    public void middleElementMinorTest(){
+        int[] array = new int[]{1,2,-6,3,4,-5};
+
+        int result = vector.minor(array);
+        Assertions.assertEquals(-6,result);
+
+    }
+
+    @Test
+    public void lastElementMinorTest(){
+        int[] array = new int[]{1,2,5,3,4,-5};
+
+        int result = vector.minor(array);
+        Assertions.assertEquals(-5,result);
+
+    }
+
+    @Test
+    public void sumatoryWithNegativeNumberTest(){
+        int[] array = new int[]{1,2,5,3,4,-5};
+
+        int result = vector.sumatory(array);
+        Assertions.assertEquals(10,result);
+
+    }
+
+    @Test
+    public void sumatoryWithoutNegativeNumberTest(){
+        int[] array = new int[]{1,2,5,3,4,5};
+
+        int result = vector.sumatory(array);
+        Assertions.assertEquals(20,result);
+
+    }
+}
